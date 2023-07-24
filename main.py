709121fa6232de48e34591d122bf4c82268a56d8
Luiz Priolli <luiz.priolli+github@acct.global>
2023-07-24 11:48:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,99 +2,118 @@ import os
 import sys
 from time import sleep
 from datetime import datetime
+import requests
 
-#colors
+# colors
 RED = "\033[1;31m"
 GREEN = "\033[0;32m"
 
-#date
-def time():
-  url = 'http://worldtimeapi.org/api/timezone/America/Sao_Paulo'
-  resposta = requests.get(url)
-  hora = datetime.fromisoformat(resposta.json()['datetime'])
-  hora = hora.strftime("%H:%M:%S - %d/%m/%Y")
-  return hora
+# List of sensitive tokens to check
+list_check = ["vtexappkey-", "my_secret_token", "api_key", "password", "access_token",
+              "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"]
 
-list_check = ["vtexappkey-", "my_secret_token", "api_key", "password", "access_token"]
-value = 0
+# Function to get the current time
+def get_time():
+    url = 'http://worldtimeapi.org/api/timezone/America/Sao_Paulo'
+    resposta = requests.get(url)
+    hora = datetime.fromisoformat(resposta.json()['datetime'])
+    hora = hora.strftime("%H:%M:%S - %d/%m/%Y")
+    return hora
 
-print("Analising code...\n")
+def analyze_code_for_tokens():
+    print("Analyzing code for sensitive tokens...\n")
+    found_warnings = False
 
-for x in list_check:
-    req = os.popen('egrep -nri "%s" * | grep -v grep | grep -v "main.py" | grep -v "list_check" '%(x)).read()
-    if x in req:
-        print("\033[3;37;40mFinded in code \033[0;37;40m  \n")
-        print(req)
-        value = value + 1
+    for token in list_check:
+        cmd = f'egrep -nri "{token}" * | grep -v grep | grep -v "main.py" | grep -v "list_check"'
+        req = os.popen(cmd).read()
 
-        try:
-            import requests
-            repo = os.popen('git config --get remote.origin.url').read()
-            branch = os.popen('git branch').read()
-            access = repo.replace('git@github.com:', 'https://github.com/')
-            url = sys.argv[1]
-            payload = '''
-            {
-                    "blocks": [
+        if token in req:
+            print(f"\033[3;37;40mFound '{token}' in code \033[0;37;40m\n")
+            print(req)
+
+            if token == "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9":
+                found_warnings = True
+                print("\033[3;33;40mWarning: JWT identified. Sending message to Slack.\033[0;37;40m")
+                # Substitua '<SLACK_WEBHOOK_URL>' pelo seu URL do webhook do Slack
+                send_slack_message("<SLACK_WEBHOOK_URL>", token)
+
+            else:
+                found_warnings = True
+                print("\033[3;33;40mWarning: Sensitive token found. Sending message to Slack.\033[0;37;40m")
+                # Substitua '<SLACK_WEBHOOK_URL>' pelo seu URL do webhook do Slack
+                send_slack_message("<SLACK_WEBHOOK_URL>", token)
+
+    if found_warnings:
+        print(RED + "Warnings found. Check Slack for details.")
+    else:
+        print(GREEN + "No warnings found.")
+
+def send_slack_message(webhook_url, token):
+    try:
+        repo = os.popen('git config --get remote.origin.url').read().strip()
+        branch = os.popen('git branch').read().strip()
+        access = repo.replace('git@github.com:', 'https://github.com/')
+
+        payload = {
+            "blocks": [
+                {
+                    "type": "header",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Found Vulnerability",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "section",
+                    "fields": [
                         {
-                            "type": "header",
-                            "text": {
-                                "type": "plain_text",
-                                "text": "Found Vulnerability",
-                                "emoji": true
-                            }
+                            "type": "mrkdwn",
+                            "text": f"*Project:*\n{repo.replace('git@github.com:quality-digital/', '').replace('.git', '')}"
                         },
                         {
-                            "type": "section",
-                            "fields": [
-                                {
-                                    "type": "mrkdwn",
-                                    "text": "*Project:*\n%s"
-                                },
-                                {
-                                    "type": "mrkdwn",
-                                    "text": "*When:* %s"
-                                },
-                                {
-                                    "type": "mrkdwn",
-                                    "text": "*Branch:* \n%s"
-                                },
-                                {
-                                    "type": "mrkdwn",
-                                    "text": "*Vuln:* \nHardcode"
-                                }
-                            ]
+                            "type": "mrkdwn",
+                            "text": f"*When:* {get_time()}"
                         },
                         {
-                            "type": "section",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": "%s"
-                            }
+                            "type": "mrkdwn",
+                            "text": f"*Branch:* \n{branch}"
                         },
                         {
-                            "type": "section",
-                            "text": {
-                                "type": "mrkdwn",
-                                "text": "%s"
-                            }
+                            "type": "mrkdwn",
+                            "text": "*Vuln:* \nHardcode"
                         }
                     ]
-                
-            }
-            '''%(repo.replace('git@github.com:quality-digital/', '').replace('.git', ''), time(), branch, req.replace('"', ''), access)
-            print(payload)
-            r = requests.post(url, data=payload)
-            print(r.text)
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": f"Found sensitive token: {token}"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": access
+                    }
+                }
+            ]
+        }
 
-        except Exception as e:
-            print("Error send message for slack.")
-            print('Log: '+ str(e))
-             
-########################################
+        r = requests.post(webhook_url, json=payload)
+        print(r.text)
 
-if value > 0:
-    sys.exit(1)
-else:
-    print(GREEN + "Pass")
-    sys.exit(0)
+    except Exception as e:
+        print("Error sending message to Slack.")
+        print('Log: ' + str(e))
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Error: Webhook URL not provided.")
+        print("Usage: python main.py <webhook_url>")
+        sys.exit(1)
+
+    analyze_code_for_tokens()
